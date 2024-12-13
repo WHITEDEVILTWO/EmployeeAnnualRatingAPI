f3c014a669a16845fe3f3091726556d30f13588e
WHITEDEVILTWO <ganesh.repalle949@gmail.com>
2024-12-13 15:33:36 +0530

    Swagger Configuration added to exsisiting application.

---
+++ EmployeeRating/src/main/java/com/example/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.swagger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+   @Bean
+   public OpenAPI awesomeAPI() {
+      // custom swagger configurations  
+        return new OpenAPI()
+                .info(new Info().title("EmployeeRatingAPI")
+                        .description("EmployeeRatingAPI description")
+                        .version("1.0"))
+                        .externalDocs(new ExternalDocumentation()
+                        .description("Ganesh, ganesh.repalle949@gmail.com")
+                        .url("http://localhost:8080/swagger-ui.html"));
+   }
+    
+}
+
